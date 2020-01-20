73a0aa616a45b92802c340a4c1cbfed973ddfae8
Jay <274903434@qq.com>
2020-01-20 22:11:45 +0800

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+import { environment as defaultEnvironment } from './environment.defaults';
+
+export const environment = {
+  ...defaultEnvironment,
+  production: true,
+  log: false,
+  flags: {
+    useNewHeader: false
+  }
+};
