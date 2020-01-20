3eb73f2a8b6fecb5f5da5feac6e83309c4d35f19
Jay <274903434@qq.com>
2020-01-20 22:42:32 +0800

    ~

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '@environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'environment-setup';
+export class AppComponent implements OnInit {
+  title: string;
+  constructor() {
+    console.log(environment);
+    this.title = 'environment-setup';
+  }
+  ngOnInit() {}
 }
